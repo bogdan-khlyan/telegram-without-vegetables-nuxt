38e70e4dd9b2a7bd80752d7d246afc06af7df4f0
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2023-10-04 10:09:26 +0300

    fix(layouts/default.vue): meta

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,6 +17,23 @@
 import BaseHeader from '@/components/common/BaseHeader'
 import BaseFooter from '@/components/common/BaseFooter'
 import {globalState} from "@/api/global/global.state.js";
+
+useHead({
+  title: 'Telegram без историй',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', property: 'description', content: 'Версия Telegram с удаленным функционалом историй' },
+    { name: 'og:title', property: 'og:title', content: 'Версия Telegram с удаленным функционалом историй' },
+    { name: 'og:description', property: 'og:description', content: 'На этой странице вы можете скачать версию Telegram с удаленным функционалом историй' },
+    { name: 'og:image', property: 'og:image', content: '/thumbnail.jpg' },
+    { name: 'og:type', property: 'og:type', content: 'website' },
+    { hid: 'keywords', name: 'keywords', content: 'telegram, без, историй, истории, отключить, выключить' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+  ]
+})
 </script>
 
 <style lang="scss" scoped>
